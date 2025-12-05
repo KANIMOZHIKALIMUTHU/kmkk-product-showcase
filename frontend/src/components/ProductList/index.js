b21ac91d1e634c21e4b485478af0d19b43a49498
KANIMOZHIKALIMUTHU <kmkk2303@gmail.com>
2025-12-05 10:55:46 +0530

    toFixed error

--- frontend/src/components/ProductList/index.js
+++ frontend/src/components/ProductList/index.js
@@ -103,7 +103,7 @@ const ProductList = () => {
                   <p className="product-desc">{p.short_desc}</p>
                   <div className="product-footer">
                     <span className="product-price">
-                      ${(product?.price || 0).toFixed(2)}
+                      $${(p.price || 0).toFixed(2)}
                     </span>
                     <button
                       className="product-btn"
